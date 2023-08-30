4f6d2722d0bf644807be7cca06fe718c8e5f78f3
ayo6706 <ayo6706@gmail.com>
2023-08-30 20:52:09 +0100

    implement array and slices

--- main.go
+++ main.go
@@ -15,7 +15,28 @@ func main() {
 	fmt.Println(name, age, phone, amount)
 	fmt.Print("hello \n")
 
+	// formatted string
 	fmt.Printf("hello %v, your age is %v \n", name, age)
 	fmt.Printf("hello %q, your age is %q \n", name, age)
 	fmt.Printf("hello %T, your age is %T \n", name, age)
+
+	// array
+	var ages [3]int = [3]int{23, 4, 5}
+	var agesTwo = [3]int{1, 2, 3}
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(agesTwo, len(agesTwo))
+
+	var names [3]string = [3]string{"ayo", "david", "dav"}
+
+	fmt.Println(names, len(names))
+
+	// slices
+	var words = []string{"he", "she", "yes"}
+	fmt.Println(words)
+	// append
+	words = append(words, "goat")
+	fmt.Println(words)
+	// rang
+	fmt.Println(words[1:3])
 }
