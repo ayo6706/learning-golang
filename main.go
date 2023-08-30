2236289e832a82039f6d299ad695d98e8d3872e7
ayo6706 <ayo6706@gmail.com>
2023-08-30 18:38:56 +0100

    variable and datatypes

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string
+	var phone int
+	var amount float64
+
+	phone = 81238484
+	amount = 33.2
+
+	age := 2
+	fmt.Println(name, age, phone, amount)
+}
